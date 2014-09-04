2da45f15a5da2269d1e2f4b3df8ca99c95683216
Rich Robinson <robinsonrc@gmail.com>
2014-09-04 11:59:49 +0100

    Update sample project (in progress)

---
+++ RCRScheduledTaskSample/RCRScheduledTaskTests/RCRScheduledTaskTests.m
@@ -0,0 +1,34 @@
+//
+//  RCRScheduledTaskTests.m
+//  RCRScheduledTaskTests
+//
+//  Created by Rich Robinson on 04/09/2014.
+//  Copyright (c) 2014 Rich Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface RCRScheduledTaskTests : XCTestCase
+
+@end
+
+@implementation RCRScheduledTaskTests
+
+//- (void)setUp {
+//    [super setUp];
+//    // Put setup code here. This method is called before the invocation of each test method in the class.
+//}
+//
+//- (void)tearDown {
+//    // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    [super tearDown];
+//}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+
+@end
