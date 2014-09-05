42c6398f33f3d559986c74b033228746b13e3059
Rich Robinson <robinsonrc@gmail.com>
2014-09-05 07:13:32 +0100

    Comment updates

--- RCRScheduledTask/RCRMinutesUtility.m
+++ RCRScheduledTask/RCRMinutesUtility.m
@@ -23,7 +23,8 @@ static NSTimeInterval const StartOfNextMinuteOffset = 0.02;
     NSTimeInterval timestamp = [[NSDate date] timeIntervalSince1970];
     NSTimeInterval startOfCurrentMinute = timestamp - fmod(timestamp, SecondsInAMinute);
     
-    // Note that we also add the additional 'NextMinuteOffset' to ensure we are definitely into the next minute, whilst also having a negligable effect on anything the user can measure (in practice the user will not see any difference between a minute starting at 0.00 and a minute starting at 0.02, which is the offset currently defined)
+    // Note that we also add the additional 'StartOfNextMinuteOffset' to ensure we are definitely 'well' into the next minute. This helps eliminate any timing errors that come from sometimes being a few thousandths of seconds either side of it - for example, with RCRScheduledTask we would often see that tasks would fire in the last few thousandths of a second of the minute before they were due to fire (and hence the minute number would be one too low when they did). Doing this will have a negligable effect on anything the user can measure - i.e. in practice the user will not see any difference between a minute starting at 0.00s and a minute starting at 0.02s, which is the offset currently defined
+    
     NSTimeInterval startOfNextMinute = startOfCurrentMinute + SecondsInAMinute + StartOfNextMinuteOffset;
     
     return [NSDate dateWithTimeIntervalSince1970:startOfNextMinute];
